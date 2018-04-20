24cd87fc63795df7be4cf0d7e3231b0dde59fc7e
Leonardo Vilarinho <leonardo-i@outlook.com>
2018-04-20 14:49:34 -0300

    add suffle method

--- src/index.js
+++ src/index.js
@@ -18,9 +18,3 @@ export const mixin = (moduleName) => {
     }
   }
 }
-
-export const shuffle = (arr) =>
-  arr
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1])
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,13 @@ import axios from 'axios'
 export const makeHttp = (url, timeout) => (
   axios.create({
     baseURL: url.substr(-1) !== '/' ? url : url.substr(0, -1),
-    timeout
+    timeout,
+    headers: { 'Content-Type': 'application/json' }
   })
 )
+
+export const shuffle = (arr) =>
+  arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1])
